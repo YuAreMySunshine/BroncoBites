cefeab60473b1a5ff081803700e7091f29a4ea04
Javi Wu <javiwu892@gmail.com>
2025-09-23 16:30:45 -0700

    javi message

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import timRoute from "./routes/tim.route.js";
 import eliRoute from "./routes/eli.route.js";
 import jaronRoute from "./routes/jaron.route.js";
+import javiRoute from "./routes/javi.route.js";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,6 +15,7 @@ app.use(cors());
 app.use("/api/tim-lee", timRoute);
 app.use("/api/eli-tolentino", eliRoute);
 app.use("/api/jaron-lin", jaronRoute);
+app.use("/api/javi-wu", javiRoute);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
