569ca1e79f7c2b4c56e96cfc10b81c8e8e421b88
t1mato <tnln3rd@gmail.com>
2025-09-23 15:47:52 -0700

    Added HTTP API

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,16 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
+import timRoute from './routes/tim.route.js';
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Middleware
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
-// Example API route
+// Routes
+app.use("/api/tim-lee", timRoute);
+
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from Express API!' });
 });
