1f581482b1d05b2db82eb001f71a828583c74d3b
t1mato <tnln3rd@gmail.com>
2025-10-07 16:03:12 -0700

    Tim route unit testing

--- Backend/test_cases/route.test.js
+++ Backend/test_cases/route.test.js
@@ -1,10 +1,10 @@
 import request from "supertest";
 import app from "../server.js";
 
-describe("GET /api/javi-wu", () => {
+describe("GET /api/tim-lee", () => {
   it("should return greeting message", async () => {
-    const res = await request(app).get("/api/javi-wu");
+    const res = await request(app).get("/api/tim-lee");
     expect(res.statusCode).toBe(200);
-    expect(res.body).toEqual({ message: "Hello this is Javi Wu" });
+    expect(res.body).toEqual({ message: "Hello this is Tim Lee" });
   });
 });
